2ff702b1ed3cfe9116e260d8d3787abceb4b30ad
Boris Souquière <souquiereb1998@gmail.com>
2023-01-21 15:10:38 +0100

    ajout du seed Jin Kazama

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Character;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        DB::table('characters')->insert([
+            'name' => 'Jin Kazama',
+            'image' => ' à faire',
+            'description' => 'Lorem ipsum',
+            'execution' => 8,
+            'poking' => 7,
+            'keepout' => 7,
+            'damage_output' => 8,
+            'throw_game' => 6,
+            'evasiveness' => 7,
+            'movement' => 7,
+            'mixup' => 7,
+            'party' => 3,
+            'space_control' => 8,
+            'okizeme' => 4
+        ]);
     }
 }
