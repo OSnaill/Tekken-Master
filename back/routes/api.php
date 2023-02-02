85c036be9e18311c04edfc10530d6efc02e1bb5f
Boris Souquière <souquiereb1998@gmail.com>
2023-02-02 10:18:27 +0100

    feature/détail d'un perso

--- back/routes/api.php
+++ back/routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/character_list', [CharacterController::class, 'characterList']);
+Route::get('/character_list/{character}', [CharacterController::class, 'characterDetails']);
